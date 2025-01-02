c7cb109f29a9aafc9b7316ca95c90b934bf831f0
BHUPESH PATEL <BHOOPS.PATEL.92372@GMAIL.COM>
2025-01-02 22:46:52 +0530

    update

--- server.js
+++ server.js
@@ -450,6 +450,6 @@ app.get("/logout", async (req, res) => {
 });
 // Start the server on port 5000 (or any available port)
 const PORT = 5000;
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
